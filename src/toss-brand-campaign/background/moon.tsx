9d6e5f547e5bc146adb3e4759a6f54391f9a4d00
YunSeong Park <qkrdbstjd95@naver.com>
2022-04-23 12:59:32 +0900

    [New Feature] moon 컴포넌트 구현

---
+++ src/toss-brand-campaign/background/moon.tsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const style = css`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+interface MoonProps {
+  opacity: number;
+  scale: { x: number; y: number };
+}
+
+const Moon: React.FC<MoonProps> = ({ opacity = 1, scale = { x: 1, y: 1 } }) => {
+  const { x, y } = scale;
+  return (
+    <img
+      css={style}
+      src="res/moon.png"
+      style={{ opacity, transform: `scale( ${x}, ${y} )` }}
+    />
+  );
+};
+
+export default Moon;
